ff99210c0c80c8eda9872293e2b5d0279557c846
Folusho Adeyemi <folushoadeyemiv@meta.com>
2023-07-18 09:18:27 -0700

    defined the many-to-many association between collections and products

--- FashionConnect_api/models/index.js
+++ FashionConnect_api/models/index.js
@@ -3,6 +3,7 @@ import { Category } from './category.js';
 import { Product } from './products.js';
 import { Review } from './reviews.js';
 import { Collection } from './collections.js';
+import { CollectionProduct } from './collectionproduct.js';
 
 User.hasMany(Collection, { as: 'collections', foreignKey: 'UserID' });
 Collection.belongsTo(User, { as: 'user', foreignKey: 'UserID' });
@@ -16,10 +17,16 @@ Product.belongsTo(Category, { as: 'category', foreignKey: 'CategoryID' });
 Product.hasMany(Review, { as: 'reviews', foreignKey: 'ProductID' });
 Review.belongsTo(Product, { as: 'product', foreignKey: 'ProductID' });
 
+Collection.belongsToMany(Product, { through: 'CollectionProduct', as: 'products', foreignKey: 'CollectionID', });
+Product.belongsToMany(Collection, { through: 'CollectionProduct', as: 'collections', foreignKey: 'ProductID', });
+
+
 User.sync();
 Category.sync();
 Product.sync();
 Review.sync();
 Collection.sync();
+CollectionProduct.sync();
+
 
 export { User, Category, Product, Review, Collection };
