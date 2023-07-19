c25723c47854ca8e3eb20bd066beef5c8900398d
Folusho Adeyemi <folushoadeyemiv@meta.com>
2023-07-19 06:04:38 -0700

    created the sequelize table for the CollectionProduct that handles the relationship/association between collections and products

---
+++ FashionConnect_api/models/collectionproduct.js
@@ -0,0 +1,19 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../database.js';
+
+export const CollectionProduct = sequelize.define('CollectionProduct', {
+  CollectionProductID: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  CollectionID: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  ProductID: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
