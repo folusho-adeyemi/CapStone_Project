2840537ec98d1d780aa87b59fd0e2a4232dc0b0a
Folusho Adeyemi <folushoadeyemiv@meta.com>
2023-07-19 11:06:01 -0700

    created an addtocollection popup component to handle the selection of components you want to specific product to

---
+++ FashionConnect_ui/src/components/AddToCollectionsPopup/AddToCollectionsPopup.jsx
@@ -0,0 +1,97 @@
+import "./AddToCollectionsPopup.css";
+import React, {useState} from "react";
+
+
+
+export default function AddToCollectionsPopup({ collections, productID, onClose }){
+
+    const [selectedCollections, setSelectedCollections] = useState([]);
+
+    const handleCollectionChange = (event, collectionID) => {
+        if (event.target.checked) {
+          setSelectedCollections((prevSelected) => [...prevSelected, collectionID]);
+        } else {
+          setSelectedCollections((prevSelected) =>
+            prevSelected.filter((id) => id !== collectionID)
+          );
+        }
+      };
+    
+    const handleAddToCollection = () => {
+    // To Update the collections with the new ProductID
+    const updatedCollections = collections.map((collection) => {
+        if (selectedCollections.includes(collection.CollectionID)) {
+            const productIDExists = collection.ProductID.includes(productID);
+      
+            // If the productID does not exist in the collection's ProductID array, add it
+            if (!productIDExists) {
+              return {
+                ...collection,
+                ProductID: [...collection.ProductID, productID],
+              };
+            }
+          }
+          return collection;;
+    });
+
+    // To convert the productid's to integer
+    updatedCollections.forEach((collection) => {
+        collection.ProductID = collection.ProductID.map(Number);
+      });
+    
+    
+    const saveUpdatedCollectionsToDatabase = async (updatedCollections) => {
+        
+        try {
+            const response = await fetch('http://localhost:3000/update-collections', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updatedCollections),
+            });
+        
+            if (!response.ok) {
+            throw new Error('Failed to update collections in the database');
+            }
+        
+            console.log('Collections updated successfully!');
+        } catch (error) {
+            console.error('Error updating collections:', error);
+            throw error;
+        
+        }
+        };
+          
+        // To Call a function to save the updated collections to the database
+        saveUpdatedCollectionsToDatabase(updatedCollections);
+    
+        // To Close the popup
+        onClose();
+      };
+   
+      return (
+        <div className="add-to-collection-popup">
+          <h2>Select Collections to Add Product</h2>
+          <ul>
+            {collections.map((collection) => (
+              <li key={collection.CollectionID}>
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={selectedCollections.includes(collection.CollectionID)}
+                    onChange={(event) =>
+                      handleCollectionChange(event, collection.CollectionID)
+                    }
+                  />
+                  <h3 className="collection-name">{collection.Name}</h3>
+                </label>
+              </li>
+            ))}
+          </ul>
+          <button onClick={handleAddToCollection}>Add to Collection</button>
+          <button onClick={onClose}>Cancel</button>
+        </div>
+      );
+    }
+    
